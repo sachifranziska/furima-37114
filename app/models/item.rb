279cf07de3a17b586934316a5fcbf884d2300e51
sachifranziska <s.shibata871@gmail.com>
2021-11-16 16:02:49 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,18 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  with_options presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ } do
+    validates :price
+  end
 end
