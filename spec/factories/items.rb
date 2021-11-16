e9e55de53b9abf4685d0a50425b3a91fc87f6ddf
sachifranziska <s.shibata871@gmail.com>
2021-11-16 20:28:35 +0900

    FactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Lorem.sentence}
+    name {Faker::Name.name}
+    info {Faker::Lorem.sentence}
+    category_id {2}
+    sales_status_id {2}
+    shipping_fee_status_id {2}
+    prefecture_id {2}
+    scheduled_delivery_id {2}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
   end
 end
