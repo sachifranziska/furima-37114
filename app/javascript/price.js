331d8842be73b313e36b6599f51df0ac0ea73a3a
sachifranziska <s.shibata871@gmail.com>
2021-11-17 20:39:28 +0900

    販売手数料と販売利益の表示

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addProfit = document.getElementById("profit");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    addProfit.innerHTML = Math.floor(inputValue * 0.9);
+  })
 });
