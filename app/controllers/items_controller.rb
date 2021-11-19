2876852ce695b0d52a730fb60f0c583d548f4120
sachifranziska <s.shibata871@gmail.com>
2021-11-19 15:21:21 +0900

    出品者以外は商品編集ページに行くとトップページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -38,5 +39,12 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless current_user.id == item.user_id
+      redirect_to action: :index
+    end
   end
 end
