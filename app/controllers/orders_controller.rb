1d7f7d4724733aa7c31fbbafe639d1eeb44ff20b
sachifranziska <s.shibata871@gmail.com>
2021-11-22 17:10:03 +0900

    createアクションとストロングパラメーター

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,17 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
   end
-  
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id]) 
+  end
 end
