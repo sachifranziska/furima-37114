c6b9b595ce54f5671a064858f965164384b98e4d
sachifranziska <s.shibata871@gmail.com>
2021-11-22 12:45:41 +0900

    購入テーブルと配送先テーブルのバリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,16 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :order_id
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Not include hyphen(-)"}
+  end
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  
 end
