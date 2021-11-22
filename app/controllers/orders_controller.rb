ec14f22f3ad9e795e1fc903603027ded089bbd4d
sachifranziska <s.shibata871@gmail.com>
2021-11-22 19:10:55 +0900

    保存出来た場合と出来なかった場合のページ遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,13 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
+      redirect_to root_path
+    else
+      render :index
     end
   end
 
